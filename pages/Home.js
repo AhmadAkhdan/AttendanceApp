99a463ef719c7aa155ded7a203fc78ab3197627b
AhmadAkhdan <AhmadAkhdan1705@gmail.com>
2026-03-13 08:47:39 +0700

    1.1

--- pages/Home.js
+++ pages/Home.js
@@ -5,25 +5,25 @@ import { MaterialIcons } from "@expo/vector-icons";
 const history = [
   { id: "1", course: "Mobile Programming", date: "2026-03-01", status: "Present" },
   { id: "2", course: "Database System", date: "2026-03-02", status: "Present" },
-  { id: "3", course: "Operating System", date: "2026-03-03", status: "Absent" },
+  { id: "3", course: "Machine Learning", date: "2026-03-03", status: "Absent" },
   { id: "4", course: "Computer Network", date: "2026-03-04", status: "Present" },
   { id: "5", course: "Web Programming", date: "2026-03-05", status: "Present" },
   { id: "6", course: "Software Engineering", date: "2026-03-06", status: "Absent" },
   { id: "7", course: "Mobile Programming", date: "2026-03-08", status: "Present" },
   { id: "8", course: "Database System", date: "2026-03-09", status: "Present" },
-  { id: "9", course: "Operating System", date: "2026-03-10", status: "Present" },
+  { id: "9", course: "Machine Learning", date: "2026-03-10", status: "Present" },
   { id: "10", course: "Computer Network", date: "2026-03-11", status: "Absent" },
   { id: "11", course: "Web Programming", date: "2026-03-12", status: "Present" },
   { id: "12", course: "Software Engineering", date: "2026-03-13", status: "Present" },
   { id: "13", course: "Mobile Programming", date: "2026-03-15", status: "Present" },
   { id: "14", course: "Database System", date: "2026-03-16", status: "Absent" },
-  { id: "15", course: "Operating System", date: "2026-03-17", status: "Present" },
+  { id: "15", course: "Machine Learning", date: "2026-03-17", status: "Present" },
   { id: "16", course: "Computer Network", date: "2026-03-18", status: "Present" },
   { id: "17", course: "Web Programming", date: "2026-03-19", status: "Present" },
   { id: "18", course: "Software Engineering", date: "2026-03-20", status: "Absent" },
   { id: "19", course: "Mobile Programming", date: "2026-03-22", status: "Present" },
   { id: "20", course: "Database System", date: "2026-03-23", status: "Present" },
-  { id: "21", course: "Operating System", date: "2026-03-24", status: "Present" },
+  { id: "21", course: "Machine Learning", date: "2026-03-24", status: "Present" },
   { id: "22", course: "Computer Network", date: "2026-03-25", status: "Present" },
   { id: "23", course: "Web Programming", date: "2026-03-26", status: "Absent" },
   { id: "24", course: "Software Engineering", date: "2026-03-27", status: "Present" },
@@ -65,8 +65,8 @@ const Home = () => {
           </View>
           <View>
             <Text style={styles.name}>Ahmad Akhdan D</Text> 
-            <Text>NIM: 123456789</Text>
-            <Text>Class: RP2B</Text>
+            <Text>NIM: 0920240006</Text>
+            <Text>Class: RPL-2B</Text>
           </View>
         </View>
 
